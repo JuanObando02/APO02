a71f2940104335b68379ced7fa9d394549180b89
JuanObando02 <124603695+JuanObando02@users.noreply.github.com>
2023-03-01 21:03:23 -0500

    Clase 5 APO27
    ejecicios actividad libro y triki

---
+++ clase5/src/codigos/GradeBook2.java
@@ -0,0 +1,13 @@
+package codigos;
+
+public class GradeBook2 {
+
+	public static void main( String args[] ) {
+		
+		GradeBook myGradeBook1 = new GradeBook("Bienvenido al curso: APO27","Juan");
+		GradeBook myGradeBook2 = new GradeBook("Bienvenido al curso: APO02","Andres ceballos");
+		myGradeBook1.displayMessage();
+		myGradeBook2.displayMessage();
+	}
+	   
+}
